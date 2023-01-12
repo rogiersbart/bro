f84491f0bc886946c44e937f6839248f97ef0330
rogiersbart <rogiers.bart@gmail.com>
2023-01-12 11:28:02 +0100

    Export year and week planning

--- R/plan.R
+++ R/plan.R
@@ -1,4 +1,14 @@
-text_plan <- function(year = lubridate::year(lubridate::today())) {
+#' Plain text year planning
+#'
+#' @param file File to write to, or "clipboard".
+#' @param year The year to use, defaults to the current year.
+#'
+#' @return The text, invisibly if written first to a file.
+#' @export
+plan_year <- function(
+    file = NULL,
+    year = lubridate::year(lubridate::today())
+  ) {
   start <- lubridate::date(paste0(year, "-01-01"))
   end <- lubridate::date(paste0(year, "-12-31"))
   days <- seq.Date(start, end, by = "day")
@@ -6,11 +16,42 @@ text_plan <- function(year = lubridate::year(lubridate::today())) {
   start2 <- which(weeks == 1)[1]
   days <- days[start2:length(days)]
   weeks <- weeks[start2:length(weeks)]
-  tibble::tibble(days = days, weeks = weeks) %>%
-    dplyr::group_by(weeks) %>%
+  txt <- tibble::tibble(days = days, weeks = weeks) |>
+    dplyr::group_by(weeks) |>
     dplyr::slice(1) |>
-    dplyr::mutate(text = paste0(stringr::str_pad(weeks, 2, pad = "0"), "|", stringr::str_sub(days, 6), " ")) %>%
-    dplyr::pull(text) %>%
-    paste0(collapse = "\n") |>
-    writeLines(file("clipboard"))
+    dplyr::mutate(text = paste0(stringr::str_pad(weeks, 2, pad = "0"), "|", stringr::str_sub(days, 6), " ")) |>
+    dplyr::pull(text) |>
+    paste0(collapse = "\n")
+  txt <- glue::glue(txt)
+  if (is.null(file)) return(txt)
+  con <- file(file)
+  writeLines(txt, con)
+  close(con)
+  invisible(txt)
+}
+
+#' Plain text week planning
+#'
+#' @param file File to write to, or "clipboard".
+#'
+#' @return The text, invisibly if written first to a file.
+#' @export
+plan_week <- function(
+  file = NULL
+) {
+  # TODO add week argument, which is current one by default to add dates?
+  days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+  txt <- glue::glue(
+    "## {days}\n",
+    "- [ ] plan\n",
+    "- [ ] ...\n",
+    "- [ ] mail\n",
+    "- [ ] teams\n",
+    "- [ ] admin{ifelse(days == 'Friday', '', '\n')}"
+  )
+  if (is.null(file)) return(txt)
+  con <- file(file)
+  writeLines(txt, con)
+  close(con)
+  invisible(txt)
 }
